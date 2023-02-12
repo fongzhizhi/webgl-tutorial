abf932082e448fa7be0daf7909743ce8f393075d
fongzhizhi <1837486996@qq.com>
2023-02-12 23:42:12 +0800

    feat: 更新案例

--- src/examples/glsl/basicVS.glsl
+++ src/examples/glsl/basicVS.glsl
@@ -3,9 +3,11 @@ attribute vec4 a_Color;     // 顶点颜色
 
 uniform mat4 u_MvpMatrix;   // 模型视图投影矩阵
 
-varying lowp vec4 v_Color;       // 顶点颜色
+varying lowp vec4 v_Color;  // 顶点颜色
 
 void main() {
     gl_Position = u_MvpMatrix * a_Position;
-    v_Color = a_Color;
+    if(bool(a_Color)) {
+        v_Color = a_Color;
+    }
 }
